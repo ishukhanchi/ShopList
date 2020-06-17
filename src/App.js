79f64f82ac3a912c574bfee28116c3f663436be6
ishukhanchi <ishukhanchi@gmail.com>
2020-06-17 11:48:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { Button, Jumbotron, Container, Toast } from "react-bootstrap";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: [],
+      show: false
+    };
+  }
+
+  updateInput(key, value) {
+    this.setState({
+      [key]: value
+    });
+  }
+  addItem() {
+    //create item with unique id
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+    //copy of current list of items
+    const list = [...this.state.list];
+
+    //add new item to list
+    list.push(newItem);
+
+    //update state with new list and reset newItem input
+    this.setState({
+      list,
+      newItem: ""
+    });
+  }
+
+  deleteItem(id) {
+    //copy current list of items
+    const list = [...this.state.list];
+
+    //filter out item being deleted
+    const updatedList = list.filter(item => item.id !== id);
+    this.setState({
+      list: updatedList
+    });
+  }
+  toggleShow = value => {
+    this.setState({ show: value });
+  };
+
+  handleClick = () => {
+    this.addItem();
+    this.toggleShow(!this.state.show);
+  }
+
+  render() {
+    return (
+      <Container className="App">
+        <Jumbotron>
+          <h1>Shopping list</h1>
+          <p>Add an Item!</p>
+          <p>
+            <input
+              type="text"
+              placeholder="Type the text here"
+              value={this.state.newItem}
+              onChange={e => this.updateInput("newItem", e.target.value)}
+            />
+          </p>
+          <p>
+            <Button variant="primary" onClick={() => this.handleClick()}>
+              Add
+            </Button>
+          </p>
+          <div className="Dot">
+            {this.state.list.map(item => {
+              return (
+                <>
+                  <Toast
+                    key={item.id}
+                    show={this.state.show}
+                    onClose={() => this.deleteItem(item.id)}
+                  >
+                    <Toast.Header>
+                      <div className="col-sm-11">
+                      <strong className="itemvalue">{item.value}</strong>
+                      </div>
+                    </Toast.Header>
+                  </Toast>
+                </>
+              );
+            })}
+          </div>
+        </Jumbotron>
+      </Container>
+    );
+  }
 }
 
 export default App;
